9ba701f6dab13ef849b2e07c132929b14fc44dfc
nikyn4ik <92479422+nikyn4ik@users.noreply.github.com>
2021-12-04 16:18:36 +0300

    log

---
+++ Diploma_2022/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma_2022
+{
+    internal class Users
+    {
+        public int id_authorization { get; set; }
+        public int id_user { get; set; }
+        public int id_role { get; set; }
+        public string login { get; set; }
+        public string password { get; set; } //or int??
+    }
+}
---
+++ Diploma_2022/Pages/Login.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Diploma_2022
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(@"Data Source=SPUTNIK; Initial Catalog=diploma_db; Integrated Security=True");
+            try
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                    sqlConnection.Open();
+
+                String query = "SELECT COUNT(*) FROM [dbo].[authorization] WHERE Login=login AND Password=password"; //@1
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@login", login.Text); //@login
+                sqlCommand.Parameters.AddWithValue("@password", password.Text);//passwprd
+
+                sqlCommand.Parameters.AddWithValue("@login", System.Data.SqlDbType.NVarChar).Value = login.Text;
+                sqlCommand.Parameters.AddWithValue("@password", System.Data.SqlDbType.NVarChar).Value = password.Text;
+
+
+                int count = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password incorrect.");
+                    this.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+    }
+}
