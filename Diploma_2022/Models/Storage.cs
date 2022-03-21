46ae84c98a94537d984c201ec24a834a113882cb
Nika <nikyn4ik@gmail.com>
2022-03-21 03:44:17 +0300

    1

--- Diploma_2022/Models/Storage.cs
+++ Diploma_2022/Models/Storage.cs
@@ -11,7 +11,7 @@ namespace Diploma_2022.Models
         public int id_storage { get; set; }
         public string name_storage { get; set; }
         public string address { get; set; }
-        public string phone_storage { get; set; } 
+        public string phone_storage { get; set; }  
         public DateTime date_of_entrance { get; set; }
         public string SAP_product_code { get; set; }
         public string remainder { get; set; }
